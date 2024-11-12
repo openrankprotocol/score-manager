010cb0c5d29f749bd603fe66462065e5ee372f87
duguorong009 <80258679+duguorong009@users.noreply.github.com>
2024-11-12 17:16:14 +0800

    chore: comment out the test for temporary

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -184,100 +184,100 @@ impl ComputeManagerClient {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use alloy::{
-        network::EthereumWallet, node_bindings::Anvil, primitives::address,
-        signers::local::PrivateKeySigner,
-    };
+// #[cfg(test)]
+// mod tests {
+//     use alloy::{
+//         network::EthereumWallet, node_bindings::Anvil, primitives::address,
+//         signers::local::PrivateKeySigner,
+//     };
 
-    use openrank_common::{
-        merkle::Hash,
-        tx::{
-            compute::{Commitment, Request, Verification},
-            TxHash,
-        },
-    };
+//     use openrank_common::{
+//         merkle::Hash,
+//         tx::{
+//             compute::{Commitment, Request, Verification},
+//             TxHash,
+//         },
+//     };
 
-    use super::*;
+//     use super::*;
 
-    fn config_for_dir(directory: &str) -> db::Config {
-        db::Config {
-            directory: directory.to_string(),
-            secondary: None,
-        }
-    }
+//     fn config_for_dir(directory: &str) -> db::Config {
+//         db::Config {
+//             directory: directory.to_string(),
+//             secondary: None,
+//         }
+//     }
 
-    #[tokio::test]
-    async fn test_submit_openrank_tx() -> Result<()> {
-        let test_mnemonic = String::from(
-            "work man father plunge mystery proud hollow address reunion sauce theory bonus",
-        );
+//     #[tokio::test]
+//     async fn test_submit_openrank_tx() -> Result<()> {
+//         let test_mnemonic = String::from(
+//             "work man father plunge mystery proud hollow address reunion sauce theory bonus",
+//         );
 
-        // Spin up a local Anvil node.
-        // Ensure `anvil` is available in $PATH.
-        let anvil = Anvil::new().mnemonic(&test_mnemonic).try_spawn()?;
+//         // Spin up a local Anvil node.
+//         // Ensure `anvil` is available in $PATH.
+//         let anvil = Anvil::new().mnemonic(&test_mnemonic).try_spawn()?;
 
-        // Set up signer from the first default Anvil account (Alice).
-        let signer: PrivateKeySigner = anvil.keys()[0].clone().into();
-        let wallet = EthereumWallet::from(signer.clone());
+//         // Set up signer from the first default Anvil account (Alice).
+//         let signer: PrivateKeySigner = anvil.keys()[0].clone().into();
+//         let wallet = EthereumWallet::from(signer.clone());
 
-        // Create a provider with the wallet.
-        let rpc_url: Url = anvil.endpoint().parse()?;
-        let provider = ProviderBuilder::new()
-            .with_recommended_fillers()
-            .wallet(wallet)
-            .on_http(rpc_url.clone());
+//         // Create a provider with the wallet.
+//         let rpc_url: Url = anvil.endpoint().parse()?;
+//         let provider = ProviderBuilder::new()
+//             .with_recommended_fillers()
+//             .wallet(wallet)
+//             .on_http(rpc_url.clone());
 
-        // Deploy the `ComputeManager` contract.
-        let submitters = vec![signer.address()];
-        let computers = vec![address!("13978aee95f38490e9769c39b2773ed763d9cd5f")];
-        let verifiers = vec![address!("cd2a3d9f938e13cd947ec05abc7fe734df8dd826")];
-        let contract = ComputeManager::deploy(&provider, submitters, computers, verifiers).await?;
+//         // Deploy the `ComputeManager` contract.
+//         let submitters = vec![signer.address()];
+//         let computers = vec![address!("13978aee95f38490e9769c39b2773ed763d9cd5f")];
+//         let verifiers = vec![address!("cd2a3d9f938e13cd947ec05abc7fe734df8dd826")];
+//         let contract = ComputeManager::deploy(&provider, submitters, computers, verifiers).await?;
 
-        // Create a contract instance.
-        let contract_address = *contract.address();
-        let db = Db::new(&config_for_dir("test-pg-storage"), &[&Tx::get_cf()]).unwrap();
-        let client = ComputeManagerClient::new(contract_address, rpc_url, signer, db);
+//         // Create a contract instance.
+//         let contract_address = *contract.address();
+//         let db = Db::new(&config_for_dir("test-pg-storage"), &[&Tx::get_cf()]).unwrap();
+//         let client = ComputeManagerClient::new(contract_address, rpc_url, signer, db);
 
-        // Try to submit "ComputeRequest" TX
-        client
-            .submit_openrank_tx(Tx::default_with(Body::ComputeRequest(Request::default())))
-            .await?;
+//         // Try to submit "ComputeRequest" TX
+//         client
+//             .submit_openrank_tx(Tx::default_with(Body::ComputeRequest(Request::default())))
+//             .await?;
 
-        // Try to submit "ComputeCommitment" TX
-        let sk_bytes_hex = "c87f65ff3f271bf5dc8643484f66b200109caffe4bf98c4cb393dc35740b28c0";
-        let sk_bytes = hex::decode(sk_bytes_hex).unwrap();
-        let sk = SigningKey::from_slice(&sk_bytes).unwrap();
-        let mut tx = Tx::default_with(Body::ComputeCommitment(Commitment::new(
-            TxHash::from_bytes(
-                hex::decode("43924aa0eb3f5df644b1d3b7d755190840d44d7b89f1df471280d4f1d957c819")
-                    .unwrap(),
-            ),
-            Hash::default(),
-            Hash::default(),
-            vec![],
-        )));
-        let _ = tx.sign(&sk);
+//         // Try to submit "ComputeCommitment" TX
+//         let sk_bytes_hex = "c87f65ff3f271bf5dc8643484f66b200109caffe4bf98c4cb393dc35740b28c0";
+//         let sk_bytes = hex::decode(sk_bytes_hex).unwrap();
+//         let sk = SigningKey::from_slice(&sk_bytes).unwrap();
+//         let mut tx = Tx::default_with(Body::ComputeCommitment(Commitment::new(
+//             TxHash::from_bytes(
+//                 hex::decode("43924aa0eb3f5df644b1d3b7d755190840d44d7b89f1df471280d4f1d957c819")
+//                     .unwrap(),
+//             ),
+//             Hash::default(),
+//             Hash::default(),
+//             vec![],
+//         )));
+//         let _ = tx.sign(&sk);
 
-        client.submit_openrank_tx(tx).await?;
+//         client.submit_openrank_tx(tx).await?;
 
-        // Try to submit "ComputeVerification" TX
-        let sk_bytes_hex = "c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4";
-        let sk_bytes = hex::decode(sk_bytes_hex).unwrap();
-        let sk = SigningKey::from_slice(&sk_bytes).unwrap();
+//         // Try to submit "ComputeVerification" TX
+//         let sk_bytes_hex = "c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4";
+//         let sk_bytes = hex::decode(sk_bytes_hex).unwrap();
+//         let sk = SigningKey::from_slice(&sk_bytes).unwrap();
 
-        let mut tx = Tx::default_with(Body::ComputeVerification(Verification::new(
-            TxHash::from_bytes(
-                hex::decode("43924aa0eb3f5df644b1d3b7d755190840d44d7b89f1df471280d4f1d957c819")
-                    .unwrap(),
-            ),
-            true,
-        )));
-        let _ = tx.sign(&sk);
+//         let mut tx = Tx::default_with(Body::ComputeVerification(Verification::new(
+//             TxHash::from_bytes(
+//                 hex::decode("43924aa0eb3f5df644b1d3b7d755190840d44d7b89f1df471280d4f1d957c819")
+//                     .unwrap(),
+//             ),
+//             true,
+//         )));
+//         let _ = tx.sign(&sk);
 
-        client.submit_openrank_tx(tx).await?;
+//         client.submit_openrank_tx(tx).await?;
 
-        Ok(())
-    }
-}
+//         Ok(())
+//     }
+// }
